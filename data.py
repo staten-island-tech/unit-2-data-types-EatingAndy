2ea03b4d6cd685cea3cdf752afaa3180645ae396
EatingAndy <yuyangh@nycstudents.net>
2025-02-17 20:57:48 -0500

    data

---
+++ data.py
@@ -0,0 +1,21 @@
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+    print(values[7]) """
+
+""" x = "this is a thing"
+y= x.split("a")
+z = y[1]
+print(y)
+print(z) """
+
+print("give me a sentence")
+x = input()
+y = x.split( )
+z = y[input]
+print(y, x)
