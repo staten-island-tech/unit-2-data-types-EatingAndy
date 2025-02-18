438af8091fe9c1e665add27176cfdfb82ef1a5ec
EatingAndy <yuyangh@nycstudents.net>
2025-02-18 17:51:37 -0500

    gcf

--- data.py
+++ data.py
@@ -14,8 +14,56 @@ z = y[1]
 print(y)
 print(z) """
 
-print("give me a sentence")
-x = input()
-y = x.split( )
-z = y[input]
-print(y, x)
+""" Print "what sentence?" in the terminal
+set variable sentence to user input in the terminal
+set variable count to the splits in sentence
+print amount of splits in count """
+
+""" print("what sentence?")
+sentence = input()
+count = sentence.split()
+print(len(count)) """
+
+""" temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
+ """
+
+""" x = "test"
+print(f"hello {x}") """
+
+""" print("number?")
+number = int(input())
+if number % 2 == 0:
+        print('even')
+else:
+    print('odd') """
+
+""" print("service?")
+answer = input()
+if answer == "bad":
+    print("0% tip")
+elif answer == "okay":
+    print("15% tip")
+elif answer == "good":
+    print("20% tip")
+elif answer == "great":
+    print("25% tip") """
+
+""" print("give number")
+x = int(input())
+factor = []
+for y in range(1, x+1):
+    if x % y == 0:
+        factor.append(y)
+print(factor) """
+
+print("give 2 numbers")
+x = int(input()), int(input())
+gcf = y in range(1, x+1)
+if x % y == 0:
+    print x
